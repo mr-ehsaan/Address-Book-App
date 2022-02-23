16d09bcff9c6c88a4323826f3fdcdddf1db7209d
muhammadehsan <muhammad.ehsan@carbonteq.com>
2022-02-23 15:36:33 +0500

    remove

--- src/Services/HomeService.js
+++
@@ -1,9 +0,0 @@
-import { Get } from "./GenericServices";
-import { toast } from "react-toastify";
-export default async function getUsers(url) {
-  console.log("URL is > ", process.env);
-  let data = await Get(`${url}`);
-  console.log("Data in Specific ", data);
-
-  return data;
-}
--- src/constants/const.jsx
+++
@@ -1,1 +0,0 @@
-export const list = ["CH", "ES", "FR", "GB"];
